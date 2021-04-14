b18669f5a563c42c7210235e73faa0beb5de299b
alexalvizuris <39359044+alexalvizuris@users.noreply.github.com>
2021-04-14 02:42:00 -0700

    added methods for setting up memo table in algorithm

--- algorithm.py
+++ algorithm.py
@@ -1,10 +1,14 @@
 import math
 from csvMethods import *
+from array import *
 
 class tspAlgorithm:
     """
     class for the algorithm I will use to solved the optimal truck route
     """
+
+    falsify = False
+    memo = [[]]
 
     def __init__(self, matrix, start):
         """
@@ -12,11 +16,55 @@ class tspAlgorithm:
         :param matrix: distance matrix
         :param start: the starting node aka the HUB
         """
-        N = matrix.size
+        nodes = matrix.size
         self.matrix = matrix
         self.start = start
         tour = []
-        falsify = False
+        self.memo = [[0 for x in range(nodes)] for y in range(2 ** nodes)]
+
+
+
+
+    def setup_memo(self, matrix, memo, start, nodes):
+        self.matrix = matrix
+        self.memo = memo
+        self.start = start
+        nodes = matrix.size
+
+        for i in range(nodes):
+            if i == start:
+                continue
+            memo[i][1 << start | 1 << i] = matrix[start][i]
+
+
+
+
+    def solve(self, matrix, memo, start, nodes):
+        self.start = start
+        self.matrix = matrix
+        self.memo = memo
+        nodes = matrix.size
+
+        # for r in range(3, nodes): finish from here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
