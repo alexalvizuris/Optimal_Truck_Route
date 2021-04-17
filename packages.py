165eb11b5958ef339a3e92564e032f2787714f5b
alexalvizuris <39359044+alexalvizuris@users.noreply.github.com>
2021-04-16 22:11:16 -0700

    added package file tp project for package class

---
+++ packages.py
@@ -0,0 +1,34 @@
+class Package:
+    """
+    Creates a package class that includes details of the package and destination
+    """
+
+    def __init__(self, ID, current_node, address, city, state, zipcode, deadline, weight, status):
+        """
+        constructor for the package class
+        :param ID: package ID
+        :param address: delivery address for pckage
+        :param deadline: time constraints for package
+        :param city: city of address for delivery
+        :param zipcode: zipcode of address for delivery
+        :param weight: weight of package
+        :param status: current status of package
+        """
+        self.ID = ID
+        self.current_node = current_node
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.deadline = deadline
+        self.weight = weight
+        self.status = status
+        self.departed = 0
+        self.delivered_at = 0
+
+    def __str__(self):
+        """
+        creates string method for package class
+        :return: package info as string
+        """
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.ID, self.current_node, self.address, self.city, self.state, self.zipcode, self.deadline, self.weight, self.status, self.departed, self.delivered_at)
